b29b63d4d20cd44ad6bcbc610483df79870c14b6
vatrat <vatrat.dev@gmail.com>
2017-10-12 05:22:58 +0000

    Add comments to new stopwatch section, change old comments

--- chord_practice.py
+++ chord_practice.py
@@ -76,15 +76,14 @@ def stopwatch():
     # Print a constantly-updating ("constantly" meaning every so often) time
     # This will be displayed at the bottom of the screen.
     try:
-        time_elapsed = 0.0 # I hope this is obvious
+        time_started = time.time() # Seconds since Epoch
+        time_elapsed = 0.0 # Set to float 0 as initial value
         time_output = "0.0" # Manually set for convenience, generated later
-        time_output_prev = "0.0" # Used so that partial ouput modification can be done
+        time_output_prev = "0.0" # Used for partial output modification
         time_len = len(time_output) # The length, generated later on
         time_len_prev = len(time_output_prev) # The length, generated later on
 
-        time_started = time.time() # Seconds since Epoch
-
-        sys.stdout.write(time_output)
+        sys.stdout.write(time_output) # Write inital value
         sys.stdout.flush()
 
         while True:
@@ -92,26 +91,37 @@ def stopwatch():
             time_output = str("%.2f" % round(time_elapsed,2)) #round 2 places
             time_len = len(time_output)
 
-            if time_output != time_output_prev:
-                if time_len == time_len_prev:
-                    print_string = time_output
+            if time_output != time_output_prev: # If rounded time changed
+                if time_len == time_len_prev: # If length hasn't changed
+                    print_string = time_output # Start at full output
+                    # Iterate through old and new time side by side
                     for ch1, ch2 in zip(time_output, time_output_prev):
-                        if ch1 == ch2:
-                            print_string = print_string[1:]
-                        else:
-                            break
+                        if ch1 == ch2: # If old and new time match here
+                            print_string = print_string[1:] # Remove 1st char
+                        else: # Difference is found
+                            break # End for loop early
+                    # Remove characters from old output, starting with first
+                    # non-matching character. Then, print section of output
+                    # that is new.
                     sys.stdout.write(("\b" * len(print_string)) + print_string)
                     sys.stdout.flush()
-                else:
+                else: # If strings are different length, number just went up
+                      # from 9 to 10, so no digits are shared at the beginning.
+                      # Therefore, go ahead and write the whole thing.
                     sys.stdout.write(("\b" * time_len_prev) + time_output)
                     sys.stdout.flush()
-            else:
-                continue
+            else: # If time is equal to previous time, don't update the prev
+                  # time values, and wait a bit to make sure the program
+                  # doesn't constantly loop through until the rounded time
+                  # changes.
+                time.sleep(0.0001)
+                continue # Skip to next while loop cycle
 
-            time_output_prev = time_output
+            time_output_prev = time_output # This cycle's current value is next
+                                           # cycle's old value
             time_len_prev = time_len
 
-            time.sleep(0.0001) # Seems reasonable to me
+            time.sleep(0.0001) # Wait a bit. This can be removed.
 
     except KeyboardInterrupt: # This is a hacky way to end the stopwatch.
         # I'd rather not set up things with threading just for this.
